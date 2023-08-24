f3f27d0fafa2a8d9f53b6b7e7276d0dc4cdf58f8
Stanislav <stazilastas@gmail.com>
2023-08-24 09:46:14 -0500

    notes

--- imageNotes/ViewController.swift
+++ imageNotes/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageOutlet: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonAction(_ sender: UIButton) {
+        imageOutlet.image = UIImage(named: "pony1")
+        
+    }
+    
 }
 
